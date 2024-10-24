015ebf67e2bdfccebad2f1d8161798dee16c7844
SafaSurti <safamushtaqsurti@gmail.com>
2024-10-24 14:04:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var input = document.getElementById('inputField')
+
+function getValue(number){
+    input.value += number
+}
+function clrAll(){
+    input.value =""
+}
+function deleteChar(){
+    input.value = input.value.slice(0,-1)
+}
+function square(){
+    input.value = input.value*input.value
+}
+function equal(){
+    input.value=eval(input.value)
+}
